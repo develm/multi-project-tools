db23296ec3c690854c415c58f88da8b2b340a12e
GitHub Action <action@github.com>
2021-02-07 02:46:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/develm/multiprojecttools/MyBundle.kt
+++ src/main/kotlin/com/github/develm/multiprojecttools/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.develm.multiprojecttools
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/develm/multiprojecttools/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/develm/multiprojecttools/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.develm.multiprojecttools.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.develm.multiprojecttools.MyBundle
 
 class MyApplicationService {
 
